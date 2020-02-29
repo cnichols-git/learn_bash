c4a041b6712b23fcfaa6e35ea52283f4d5fc7e7a
Chris Nichols <cnicols.mail@gmail.com>
2020-02-29 17:25:52 -0600

    updated be_safe.sh

--- be_safe.sh
+++ be_safe.sh
@@ -1,15 +1,42 @@
 # Boilerplate
 #
 #define why we are doing this
-SAFE=echo
+# SAFE=echo
 
 #define what the EUID
+# SUDO=
+# if  [ "${EUID}" -ne 0 ]
+# then
+#     SUDO=sudo
+# fi
+# echo $SUDO
+
+#provide a clear example of running this snippet
+# ${SAFE} ${SUDO} run code here
+
+#------------#
+# eol - End Of Line
+# - this indicates the end of a copypasta section
+# SAFE - allows you to copy the script into a terminal
+# and have it print out what it will do
+#------------#------------#
+
+SAFE=echo
+
+# define what the EUID
 SUDO=
 if  [ "${EUID}" -ne 0 ]
 then
-    SUDO=sudo
+   SUDO=sudo
 fi
 echo $SUDO
+#eol
 
-#provide a clear example of running this snippet
-${SAFE} ${SUDO} run code here
+
+# print this thing
+echo "this thing"
+#eol
+
+# print this thing with sudo privledges
+${SUDO} echo "this thing"
+#eol
